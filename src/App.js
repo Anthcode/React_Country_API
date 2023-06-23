5fe9338262b85a7baf6f25156e8ce72d770b0e9c
Anthcode <108927171+Anthcode@users.noreply.github.com>
2023-06-23 17:44:44 +0200

    add Header and Search component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import './style.css';
+import Header from './components/Header';
 
 export default function App() {
   return (
     <div className="App">
       <h1>Counrty API</h1>
-      <div className="input-wrap">
-        <input type="text" placeholder="Search country: " />
-      </div>
+      <Header />
     </div>
   );
 }
