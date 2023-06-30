17701f0b6685783f9b7426af68138047a992a737
Anthcode <108927171+Anthcode@users.noreply.github.com>
2023-06-30 20:51:36 +0200

    style css

--- src/API_data/GetApi.js
+++ src/API_data/GetApi.js
@@ -1,27 +1,30 @@
 import React, { useState } from 'react';
 import { useQuery } from '@tanstack/react-query';
-import fetch from './fetch'
+import fetch from './fetch';
 
 const GetApi = () => {
   const [addData, setAddData] = useState(5);
 
   const { status, data } = useQuery(['name'], () =>
-  fetch("https://restcountries.com/v3.1/all/?fields=name")
-);
-if (status === "loading") return <p>Loading...</p>;
-if (status === "error") return <p>Error :(</p>;
+    fetch('https://restcountries.com/v3.1/all/?fields=name,tld,flag')
+  );
+  if (status === 'loading') return <h2>Loading...</h2>;
+  if (status === 'error') return <h2>Error :(</h2>;
 
-console.log(data);
+  console.log(data);
   return (
     <>
       <ul>
-        {data.slice(0, addData).map((el,idx) => (
-         
-            <li key={idx} > {el.name.common}</li>
-          
+        {data.slice(0, addData).map((el) => (
+          <li key={el.tld[0]}>
+            <div className="flag">{el.flag}</div>
+            {el.name.common}
+          </li>
         ))}
       </ul>
-      <button onClick={() => setAddData(addData + 5)}>Add Next</button>
+      <button className="btn-next" onClick={() => setAddData(addData + 5)}>
+        Add Next
+      </button>
     </>
   );
 };
