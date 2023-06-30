637721949aba0ce440223b7d3266cc5f3d4280c9
Anthcode <108927171+Anthcode@users.noreply.github.com>
2023-06-30 20:21:28 +0200

    export fetch fn to one file , Query[name] data with fields endpoint

--- src/API_data/GetApi.js
+++ src/API_data/GetApi.js
@@ -1,28 +1,24 @@
 import React, { useState } from 'react';
 import { useQuery } from '@tanstack/react-query';
+import fetch from './fetch'
 
 const GetApi = () => {
   const [addData, setAddData] = useState(5);
-  // Fetcher function
-  const getCountriesData = async () => {
-    const req = await fetch('https://restcountries.com/v3.1/all');
-    const data = await req.json();
-    return data.map((country) => country.name.common);
-  };
-  // Using the hook
-  const { data, error, isLoading } = useQuery(['name'], getCountriesData);
-  // Error and Loading states
-  if (error) return <div>Request Failed {error}</div>;
-  if (isLoading) return <div>Loading...</div>;
-  // Show the response if everything is fine
 
+  const { status, data } = useQuery(['name'], () =>
+  fetch("https://restcountries.com/v3.1/all/?fields=name")
+);
+if (status === "loading") return <p>Loading...</p>;
+if (status === "error") return <p>Error :(</p>;
+
+console.log(data);
   return (
     <>
       <ul>
-        {data.slice(0, addData).map((el) => (
-          <div key={data.queryKey}>
-            <li> {el}</li>
-          </div>
+        {data.slice(0, addData).map((el,idx) => (
+         
+            <li key={idx} > {el.name.common}</li>
+          
         ))}
       </ul>
       <button onClick={() => setAddData(addData + 5)}>Add Next</button>
--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
-const queryClient = new QueryClient();
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 root.render(
   <StrictMode>
