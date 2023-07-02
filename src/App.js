7821cd3d52ca52723171b51ea265d4eca0a0303d
Anthcode <108927171+Anthcode@users.noreply.github.com>
2023-07-02 13:52:11 +0200

    3 files modified, 2 deleted

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import './style.css';
 import Header from './components/Header';
-import GetApi from './API_data/GetApi';
+
 
 
 export default function App() {
   return (
+    <>
     <div className="App">
       <h1>Counrty API</h1>
       <Header />
-      <GetApi />
     </div>
+    <Footer />
+    </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,27 +1,15 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import Footer from './components/Footer'
+
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false,
-    },
-  },
-});
+
 
 root.render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
       <App />
-      <Footer/>
-      <ReactQueryDevtools initialIsOpen />
-    </QueryClientProvider>
   </StrictMode>
 );
