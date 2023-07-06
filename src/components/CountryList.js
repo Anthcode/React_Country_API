6a30811d4aba41e28f5a0e4bba99a81702748e31
Anthcode <108927171+Anthcode@users.noreply.github.com>
2023-07-06 15:15:08 +0200

    handleCountry Fn

--- src/components/CountryList.js
+++ src/components/CountryList.js
@@ -2,19 +2,24 @@ import React, {useState} from 'react';
 
 export default function CountryList({countries}) {
   const [addData, setAddData] =useState(10)
-  const [countrySelect, setCountrySelect] = useState('')
+  const [countrySelect, setCountrySelect] = useState()
+
+  const handleCountry =(name) =>{
+    setCountrySelect(name)
+
+  }
 
   return (
     <>
 {countrySelect &&
     <div className="CountryWrapp">
-    <div className="CountryInfo">{countrySelect} </div><div>x</div>
-    </div>}
+    <div className="CountryInfo">{countrySelect.flag} {countrySelect.name.common} </div><div>x</div>
+    </div>} 
 
     <div className="CountryList">
 <ul>
     {countries.slice(0, addData).map((country)=>(
-     <li key={country.name.common} onClick={(e)=>setCountrySelect(e.currentTarget.innerText)}>{country.flag} {country.name.common}</li>
+     <li key={country.name.common} onClick={()=>handleCountry(country)}>{country.flag} {country.name.common}</li>
     ))}
     </ul>
     <button className="btn-next" onClick={() => setAddData(addData + 5)}>
