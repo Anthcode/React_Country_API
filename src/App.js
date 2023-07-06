f6985d6a0c00f47f574ca0c4ce6a7bff2183d5c7
Anthcode <108927171+Anthcode@users.noreply.github.com>
2023-07-06 13:23:32 +0200

    add useRef to get Country data once

--- src/App.js
+++ src/App.js
@@ -1,37 +1,39 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './style.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import CountryList from './components/CountryList'
-
+import CountryList from './components/CountryList';
 
 export default function App() {
-
-  const URL = "https://restcountries.com/v3.1/all?fields=name,flag";
+  const URL = 'https://restcountries.com/v3.1/all?fields=name,flag';
   const [countries, setCountries] = useState([]);
-  const [filter, setFilter]= useState([])
+  const [filter, setFilter] = useState([]);
+  const dataRan = useRef(false);
 
   useEffect(() => {
-    const getData = async () => {
-      try {
-        const res = await fetch(URL);
-        const resp = await res.json();
-        resp.sort((a,b)=> a.name.common>b.name.common?1:-1)
-        setCountries(resp);
-        setFilter(resp)
-      } catch (err) {
-        console.log(err);
-      }
-    };
-    getData();
+    if (dataRan.current === true) {
+      const getData = async () => {
+        try {
+          const res = await fetch(URL);
+          const resp = await res.json();
+          resp.sort((a, b) => (a.name.common > b.name.common ? 1 : -1));
+          setCountries(resp);
+          setFilter(resp);
+        } catch (err) {
+          console.log(err);
+        }
+      };
+      getData();
+    }
+    return () => (dataRan.current = true);
   }, []);
 
   return (
     <>
       <div className="App">
         <h1>Counrty API</h1>
-        <Header   setCountries={setCountries} filter={filter}  />
-        <CountryList countries={countries} filter={filter}/>
+        <Header setCountries={setCountries} filter={filter} />
+        <CountryList countries={countries} filter={filter} />
       </div>
       <Footer />
     </>
