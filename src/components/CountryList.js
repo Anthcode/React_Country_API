90766a76030fee5aed7bcaf2ee1929737eafb37c
Anthcode <108927171+Anthcode@users.noreply.github.com>
2023-07-17 12:36:31 +0200

    styles css

--- src/components/CountryList.js
+++ src/components/CountryList.js
@@ -2,28 +2,26 @@ import React, {useState} from 'react';
 import Country from './Country'
 
 export default function CountryList({countries}) {
-  const [addData, setAddData] =useState(10)
+  const [addData, setAddData] =useState(20)
   const [countrySelect, setCountrySelect] = useState()
-
   const handleCountry =(name) =>{
     setCountrySelect(name)
-
   }
-
   return (
-    <>
-{countrySelect && <Country countrySelect={countrySelect} setCountrySelect={setCountrySelect}/>} 
-
-    <div className="CountryList">
-<ul>
-    {countries.slice(0, addData).map((country)=>(
-     <li key={country.name.common} onClick={()=>handleCountry(country)}>{country.flag} {country.name.common}</li>
-    ))}
-    </ul>
-    <button className="btn-next" onClick={() => setAddData(addData + 5)}>
-        Add Next
-      </button> 
-    </div>
+  <>
+    {countrySelect && <Country countrySelect={countrySelect} setCountrySelect={setCountrySelect}/>} 
+      <div className="CountryList">
+        <ul className="NameList">
+          {countries.slice(0, addData).map((country) => (
+            <li key={country.name.common} onClick={() => handleCountry(country)}>
+              {country.flag} {country.name.common}
+            </li>
+          ))}
+        </ul>
+        <button className="btn-next" onClick={() => setAddData(addData + 5)}>
+          Add Next
+        </button>
+      </div>
 </>
   );
 }
